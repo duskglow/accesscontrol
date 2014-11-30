399ee825359364d3b3c5015eada03363d19447de
Russell Miller <rmiller@rmiller-Lenovo-IdeaPad-Y530.(none)>
2014-11-30 13:21:00 -0800

    Add support for emailing when someone gets in, and logging too.
    Support for both of these is somewhat rudimentary.

--- linino/bin/access.py
+++ linino/bin/access.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import smtplib 
 import sys
 
 sys.path.insert(0, '/usr/lib/python2.7/bridge/')                                          
@@ -19,6 +20,28 @@ json_users.close();
 
 bc = bridgeclient()
 
+TO='access@ctrlh.org'
+FROM= zone + '@ctrlh.org'
+SUBJECT='Someone has entered ' + zone
+
+def log(str):
+	fd = open("/mnt/sda1/ac/log/logfile", 'w')
+	fd.write(str + '\n')
+	fd.close()
+	
+def send_email( str ):
+	try:
+		smtpserver = smtplib.SMTP("mail.ctrlh.org", 26)
+		smtpserver.ehlo()
+		header = 'To: ' + TO + '\nFrom: ' + FROM + '\nSubject: ' + SUBJECT + '\n'
+		msg = header + '\n' + str + ' \n\n'
+		smtpserver.sendmail(FROM, TO, msg)
+		smtpserver.close()
+	except smtplib.SMTPException:
+		# couldn't send.
+		log("Error connecting to SMTP server, continuing.")
+		pass	
+	
 while True:
 	code = bc.get("CODE")
 	if (code):
@@ -27,5 +50,7 @@ while True:
 			continue
 		if (users[code][zone] == "Yes"):
 			bc.mailbox("O")
+			name = users[code].get("Name")
+			log("Allowed user " + name)
+			send_email(name)
 		bc.delete("CODE")
-
