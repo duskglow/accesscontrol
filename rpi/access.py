fe15b28192d7d45429941c29f791b93e662d2944
Kenny <aSmig+github@romhat.net>
2015-02-10 16:29:12 -0800

    Refactor, first pass

--- rpi/access.py
+++ rpi/access.py
@@ -2,89 +2,73 @@
 #
 # vim: et ai sw=4
 
-#import daemon  # This is not used anymore
 import RPi.GPIO as GPIO
 import sys,time
 import signal
 import subprocess
-
 import json
 import smtplib
+import threading
+import syslog
 
-conf_dir = './conf/'
-
+debug_mode = False
+conf_dir = "./conf/"
 GPIO.setmode(GPIO.BCM)
+syslog.openlog("accesscontrol", syslog.LOG_PID, syslog.LOG_AUTH)
+syslog.syslog("Initializing")
 
+# Globalize some variables for later
 zone = None
 users = None
 config = None
-locker = None
 active = None
+locker = None
+lockerzone = None
+open_hours = False
+
+def load_json(filename):
+    file_handle = open(filename)
+    config = json.load(file_handle)
+    file_handle.close()
+    return config
 
 def read_configs():
-    json_zone = open(conf_dir + 'zone.json')
-    jzone = json.load(json_zone)
-    json_zone.close()
-
-    global zone 
+    global zone, users, config, locker, lockerzone, active
+    jzone = load_json(conf_dir + "zone.json")
+    users = load_json(conf_dir + "users.json")
+    config = load_json(conf_dir + "config.json")
+    locker = load_json(conf_dir + "locker.json")
     zone = jzone["zone"]
-
-    global users
-    json_users = open(conf_dir + 'users.json')
-    users = json.load(json_users)
-    json_users.close()
-
-    global config
-    json_config = open(conf_dir + 'config.json')
-    config = json.load(json_config)
-    json_config.close()
-
-    global locker
-    json_locker = open(conf_dir + 'locker.json')
-    locker = json.load(json_locker)
-    json_locker.close()
-
-    if (zone == "locker"):
-        lockerzone = jzone["lockerzone"]
-
-    # initialize some variables, should do this in a def.
-    global active
+    lockerzone = jzone["lockerzone"]
     active = config[zone]["Active"]
-    if active == 'High':
-        active = GPIO.HIGH
-    elif active == 'Low':
-        active = GPIO.LOW
-    else:
-        print ("Invalid active directive for zone " + zone)
-        sys.exit(0)
 
-def initGPIO():
+def init_GPIO(gpio):
+    GPIO.setup(gpio, GPIO.OUT)
+    GPIO.output(gpio, active^1)
+
+def setup_GPIOs():
     if (zone == "locker"):
-        for relay in iter(locker):
-            r = int(locker[relay]["Relay"])
-            GPIO.setup(r, GPIO.OUT)
-            GPIO.output(r, True)
+        for number in iter(locker):
+            init_GPIO(locker[number]["Relay"])
     else:
-        r = int(config[zone]["Relay"])
-        GPIO.setup(r, GPIO.OUT)
-        GPIO.output(r, active^1)
+        init_GPIO(config[zone]["Relay"])
 
 def rehash(a=None, b=None):
+    report("Reloading configuration files")
     read_configs()
-    initGPIO()
+    setup_GPIOs()
 
-def triggerRelay(r, open_hours=False):
-    relay = int(r)
-    if (zone == "locker"):
-        GPIO.output(relay, False)
-        time.sleep(2)
-        GPIO.output(relay, True)
-    else:
-        GPIO.output(relay, active)
-        if (open_hours):
-            return True
-        time.sleep(0.1)
-        GPIO.output(relay, active^1)
+def triggerRelay(gpio_number, leave_open=False):
+    gpio_number = gpio_number
+    GPIO.output(gpio_number, active)
+    if leave_open:
+        return True
+    time.sleep(config[zone]["open_delay"])
+    GPIO.output(gpio_number, active^1)
+
+def debug(message):
+    if debug_mode:
+        print message
 
 def decodeStr(line):
     tbstr = line.split(" : ")[1]
@@ -92,128 +76,116 @@ def decodeStr(line):
     bstr = tbstr[0:24] + tbstr[30:]
 
     if len(bstr) != 26:
-        #print("Incorrect string length received: " + str(len(bstr)))
-        #print(":" + bstr + ":")
+        debug("Incorrect string length received: %i" % len(bstr))
+        debug(":%s:" % bstr)
         return -1
-    lparity = bstr[0]
-    facility = bstr[1:9]
-    id = bstr[9:25]
-    rparity = bstr[25]
+    lparity = int(bstr[0])
+    facility = int(bstr[1:9], 2)
+    user_id = int(bstr[9:25], 2)
+    rparity = int(bstr[25])
     lpstr = bstr[1:13]
     rpstr = bstr[13:25]
 
-    #print(line)
-    #print(lparity + " " + facility + " " + id + " " + rparity)
+    debug(line.rstrip())
+    debug("%i %i %i %i" % (lparity, facility, user_id, rparity))
     # check parity
-    pcnt = 0
+    calculated_lparity = 1
+    calculated_rparity = 1
     for iter in range(0, 12):
-        if (lpstr[iter] == "1"):
-            pcnt += 1
-    if (pcnt % 2 == lparity):
-        #print("Left parity Error in received string!")
+        calculated_lparity ^= int(lpstr[iter])
+        calculated_rparity ^= int(rpstr[iter])
+    if (calculated_lparity != lparity or calculated_rparity != rparity):
+        debug("Parity error in received string!")
         return -1
 
-    pcnt = 0
-    for iter in range(0, 12):
-        if (rpstr[iter] == "1"):
-            pcnt += 1
-    if (pcnt % 2 == rparity):
-        #print("Right parity Error in received string!")
-        return -1
+    user_id = str(user_id)
+    debug("Successfully decoded id %s" % user_id)
+    return user_id
 
-    return int(id, 2)
-
-def send_email(subject, body):
+def send_email(subject, body=""):
     try:
         emailfrom = config["emailfrom"]
         to = config["emailto"]
-        server = config["emailserver"]
-        port = int(config["emailport"])
-        smtpserver = smtplib.SMTP(server, port)
+        smtpserver = smtplib.SMTP(config["emailserver"], config["emailport"])
         smtpserver.ehlo()
-        header = 'To: ' + to + '\nFrom: ' + emailfrom + '\nSubject: ' + subject + '\n'
-        msg = header + '\n' + body + ' \n\n'
+        header = "To: %s\nFrom: %s\nSubject: %s\n" % (to, emailfrom, subject)
+        msg = "%s\n%s\n\n" % (header, body)
         smtpserver.sendmail(emailfrom, to, msg)
         smtpserver.close()
     except smtplib.SMTPException:
         # couldn't send.
         pass
 
-def process_line(line):
-    global retstr
-    global repeat_read_timeout
-    global repeat_read_count
-    global open_hours
+def report(subject):
+    syslog.syslog(subject)
+    if config and config.get("emailserver"):
+        t = threading.Thread(target=send_email, args=(subject,))
+        t.start()
 
-    ret = decodeStr(line)
-    if (ret == -1):
-        #print("Received an invalid or corrupted line")
-        return 1
+def process_wiegand_line(line):
+    global card_id, repeat_read_timeout, repeat_read_count, open_hours
 
-    last_id = retstr
-    retstr = str(ret)
-    if (users.get(retstr) is None):
-        subject = "Card " + retstr + " presented and access was denied."
-        body = ""
-        send_email(subject, body)
-        return 1
+    last_id = card_id
+    card_id = decodeStr(line)
+    if (card_id == -1):
+        debug("Received an invalid or corrupted line")
+        return
+    if (users.get(card_id) is None):
+        report("Card %s presented at %s and access was denied" % (card_id, zone))
+        return
+
     now = time.time()
-    if (retstr == last_id and now <= repeat_read_timeout):
+    if (card_id == last_id and now <= repeat_read_timeout):
         repeat_read_count += 1
     else:
         repeat_read_count = 0
         repeat_read_timeout = now + 120
     if (zone != "locker"):
-        if (users[retstr][zone] == "Yes"):
+        if (users[card_id][zone] == "Yes"):
             if (repeat_read_count >= 3):
                 open_hours = True
                 repeat_read_count = 0
-                send_email("DOOR IS LOCKED OPEN", "")
+                report("%s is unlocked" % zone)
             else:
                 if (open_hours == True):
-                    send_email("DOOR IS NORMAL", "")
+                    report("%s is locked" % zone)
                 open_hours = False
             triggerRelay(config[zone]["Relay"], open_hours)
-            name = users[retstr].get("Name")
-            first, last = name.split(" ")
-            lastinitial = last[0]
-            subject = first + " " + lastinitial + ". has entered zone " + zone
-            body = ""
-            send_email(subject, body)
+            first, last = users[card_id]["Name"].split(" ")
+            report("%s %s. has entered %s" % (first, last[0], zone))
         else:
-            subject = "Card " + retstr + " presented and access was denied."
-            body = ""
-            send_email(subject, body)
+            report("Card %s presented at %s and access was denied" % (card_id, zone))
     else:
-        if (users[retstr].get("locker") is None):
-            return 1
-        userlocker = users[retstr]["locker"]
+        if (users[card_id]["locker"] is None):
+            return
+        userlocker = users[card_id]["locker"]
         if (locker[userlocker]["Zone"] == lockerzone):
             triggerRelay(locker[userlocker]["Relay"])
 
 def cleanup(a=None, b=None):
+    report("Shutting down")
     GPIO.setwarnings(False)
     GPIO.cleanup()
     proc.terminate()
     sys.exit(0)
 
+# Catch some exit signals
+signal.signal(signal.SIGINT, cleanup)   # Ctrl-C
+signal.signal(signal.SIGTERM, cleanup)  # killall python
+
+# Reload config files
+signal.signal(signal.SIGHUP, rehash)    # killall -HUP python
+signal.signal(signal.SIGUSR2, rehash)   # killall -USR2 python
+
 rehash()
-signal.signal(signal.SIGINT, cleanup)   # Ctrl-C will try to exit cleanly
-signal.signal(signal.SIGTERM, cleanup)  # default kill signal (killall python)
-signal.signal(signal.SIGHUP, rehash)    # reload config files (killall -HUP python)
-signal.signal(signal.SIGUSR2, rehash)   # reload config files (killall -USR2 python)
-
-#daemon.daemonize("/var/run/access.pid")
-
-retstr = ''
+card_id = ""
 repeat_read_timeout = time.time()
 repeat_read_count = 0
-open_hours = False
-while (True):
-    proc = subprocess.Popen(['./wiegand'],stdout=subprocess.PIPE)
+while True:
+    proc = subprocess.Popen(["./wiegand"],stdout=subprocess.PIPE)
     try:
-        for line in iter(proc.stdout.readline, ''):
-            process_line(line)
+        for line in iter(proc.stdout.readline, ""):
+            process_wiegand_line(line)
     except Exception as inst:
         print inst
         proc.terminate()
